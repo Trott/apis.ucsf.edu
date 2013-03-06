e5300ad210eec515f241abc6100c4af1f7a4dd72
Rich Trott <rtrott@gmail.com>
2013-03-05 16:06:49 -0800

    whoops, remove port

--- static/UCSF.People.js
+++ static/UCSF.People.js
@@ -1,7 +1,9 @@
 var UCSF = UCSF || {};
 
+//TODO: Check for jQuery or Zepto. Include Zepto if neither found.
+
 UCSF.People = {
     search: function (options, callback) {
-        $.getJSON('http://apis.ucsf.edu:3000/people/search',options,callback);
+        $.getJSON('http://apis.ucsf.edu/people/search',options,callback);
     }
 };
