6f7f3a9335d1e7b8aea4d69af0e93282f7db4598
Rich Trott <rtrott@gmail.com>
2013-03-06 17:16:06 -0800

    jshint compliance

--- static/UCSF.Person.js
+++ static/UCSF.Person.js
@@ -1,45 +1,48 @@
-var UCSF = UCSF || {};
+var UCSF = {
 
-UCSF.createCORSRequest = function (method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-        // XHR for Chrome/Firefox/Opera/Safari.
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-        // XDomainRequest for IE.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        // CORS not supported.
-        xhr = null;
+    createCORSRequest: function (method, url) {
+        "use strict";
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            // XHR for Chrome/Firefox/Opera/Safari.
+            xhr.open(method, url, true);
+        } else if (typeof XDomainRequest !== "undefined") {
+            // XDomainRequest for IE.
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        } else {
+            // CORS not supported.
+            xhr = null;
+        }
+        return xhr;
+    },
+
+    makeCORSRequest: function (url, options, success, failure) {
+        "use strict";
+        //TODO: add options to url
+
+        var xhr = UCSF.createCORSRequest('GET', url);
+        if (!xhr) {
+            window.console.log('Error: CORS not supported');
+            return;
+        }
+
+        // Response handlers.
+        xhr.onload = function () {
+            success(JSON.parse(xhr.responseText));
+        };
+        xhr.onerror = function () {
+            failure(JSON.parse(xhr.responseText));
+        };
+        xhr.send();
+    },
+
+    Person: {
+        search: function (options, callback) {
+            "use strict";
+            UCSF.makeCORSRequest('http://apis.ucsf.edu.trott.jit.su/person/search', options, callback);
+        }
     }
-    return xhr;
 };
 
-UCSF.makeCORSRequest = function (url, options, success, failure ) {
-
-    //TODO: add options to url
-
-    var xhr = UCSF.createCORSRequest('GET', url);
-    if (!xhr) {
-        window.console.log('Error: CORS not supported');
-        return;
-    }
-
-    // Response handlers.
-    xhr.onload = function(e) {
-        success(JSON.parse(xhr.responseText));
-    };
-    xhr.onerror = function(e) {
-        failure(JSON.parse(xhr.responseText));
-    };
-    xhr.send();
-};
-
-UCSF.Person = {
-    search: function (options, callback) {
-        UCSF.makeCORSRequest('http://apis.ucsf.edu.trott.jit.su/person/search', options, callback);
-    }
-};
-
-//TODO: minify with uglify
+//TODO: use grunt to run through jshint and minify with uglify
