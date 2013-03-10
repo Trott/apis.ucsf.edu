ddba227ccc99a71a03c784ce46b80c1d8049d250
Rich Trott <rtrott@gmail.com>
2013-03-10 09:28:40 -0700

    at grunt for linting and minification of served JS

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        predef: ['XDomainRequest'],
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      static: ['static/UCSF.Person.src.js']
+    },
+    uglify: {
+      static: {
+        files: {
+          'static/UCSF.Person.js': ['static/UCSF.Person.src.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['jshint:static', 'uglify:static']);
+};
---
+++ static/UCSF.Person.src.js
@@ -0,0 +1,63 @@
+var UCSF = {
+
+    serialize: function(obj, prefix) {
+        "use strict";
+        var str = [];
+        for(var p in obj) {
+            var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+            str.push(typeof v === "object" ?
+                this.serialize(v, k) :
+                encodeURIComponent(k) + "=" + encodeURIComponent(v));
+        }
+        return str.join("&");
+    },
+
+    createCORSRequest: function (method, url) {
+        "use strict";
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            // XHR for Chrome/Firefox/Opera/Safari.
+            xhr.open(method, url, true);
+        } else if (typeof XDomainRequest !== "undefined") {
+            // XDomainRequest for IE.
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        } else {
+            // CORS not supported.
+            xhr = null;
+        }
+        return xhr;
+    },
+
+    makeCORSRequest: function (url, options, success, failure) {
+        "use strict";
+        var separator = url.indexOf('?')===-1 ? '?' : '&';
+        var urlWithOptions =  url + separator + this.serialize(options);
+
+        var xhr = UCSF.createCORSRequest('GET', urlWithOptions);
+        if (!xhr) {
+            //TODO: CORS not supported. Probably IE7 (or maybe Opera Mini). Look at flXHR polyfill?
+            return;
+        }
+
+        // Response handlers.
+        xhr.onload = function () {
+            success(JSON.parse(xhr.responseText));
+        };
+        xhr.onerror = failure;
+        xhr.send();
+    },
+
+    Person: {
+        search: function (options, success, failure ) {
+            "use strict";
+            failure = failure || function () {};
+            UCSF.makeCORSRequest(
+                'http://apis.ucsf.edu.trott.jit.su/person/search',
+                options,
+                success,
+                failure
+            );
+        }
+    }
+};
