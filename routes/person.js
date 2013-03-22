032c984bacf34987bda0e66e169e4770b6ea2d3d
Rich Trott <rtrott@gmail.com>
2013-03-21 21:15:36 -0700

    do not send results if CORS origin header did not match hostname from apikey lookup

--- routes/person.js
+++ routes/person.js
@@ -5,11 +5,9 @@ exports.search = function(req, res) {
     "use strict";
 
     // If no CORS header indicating API key matched origin, return nothing.
-    // TODO: Make this actually, you know, work.
-    // TODO: This file doesn't log.
-    // if (! res.get('access-control-request-method')) {
-    //     return res.send(200);
-    // }
+    if (! res.get('access-control-request-method')) {
+        return res.send(200);
+    }
 
     var directoryOptions = {
       host: "directory.ucsf.edu",
