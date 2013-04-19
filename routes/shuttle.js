2ceb283bdcb15b2475fd655b8c645bf5d91d859c
Rich Trott <rtrott@gmail.com>
2013-04-19 13:57:19 -0700

    seriously, make the walk distance shorter

--- routes/shuttle.js
+++ routes/shuttle.js
@@ -116,7 +116,7 @@ exports.plan = function(req, res) {
 
     var otpOptions = {
         host: "apis.ucsf.edu",
-        path: "/opentripplanner-api-webapp/ws/plan?mode=TRANSIT,WALK&min=QUICK&maxWalkDistance=240&walkSpeed=1.341&",
+        path: "/opentripplanner-api-webapp/ws/plan?mode=TRANSIT,WALK&min=QUICK&maxWalkDistance=30&walkSpeed=1.341&",
         port: 8080,
         headers: {'Content-Type':'application/json'}
     };
