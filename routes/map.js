2150bd6c83d0b1dcaea0269e10476e3af22ec191
Rich Trott <rtrott@gmail.com>
2013-06-21 14:17:38 -0700

    tile implementation

--- routes/map.js
+++ routes/map.js
@@ -1,11 +1,29 @@
+var path = require('path');
+
 exports.tile = function(req, res) {
     "use strict";
 
-    var path = __dirname + '../static/map_tiles/' + req.params.x +
-               '/' + req.params.y +
-               '/' + req.params.zoom + '.png';
+    var isInteger = function(string) {
+        return isFinite(parseInt(string, 10));
+    };
 
-    res.sendfile(path, function(err) {
-        console.dir(err);
-    });
+    var x = req.params.x,
+        y = req.params.y,
+        zoom = req.params.zoom;
+
+    if (isInteger(x) && isInteger(y) && isInteger(zoom)) {
+
+        var pathRoot = path.normalize(__dirname + '/../static/map_tiles/');
+        var sendfileOptions = {root: pathRoot};
+        var tilePath =  x + '/' + y + '/' + zoom + '.png';
+
+
+        res.sendfile(tilePath, sendfileOptions, function(err) {
+            if (err && err.status === 404) {
+                res.sendfile('blank.png', sendfileOptions);
+            }
+        });
+    } else {
+        res.send(404);
+    }
 };
