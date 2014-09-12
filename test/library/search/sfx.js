ab57e0e961257032045ac1e4cd84c4f947bae0b7
Rich Trott <rtrott@gmail.com>
2014-09-12 14:58:43 -0700

    refactor test

--- test/library/search/sfx.js
+++ test/library/search/sfx.js
@@ -13,33 +13,29 @@ var it = lab.test;
 
 describe('sfx', function () {
 
-	var sfxHelper = function (q, callback) {
-		sfx.search(q, callback);
-	};
-
 	it('returns an empty result if no search term provided', function (done) {
-		sfxHelper('', function (result) {
+		sfx.search('', function (result) {
 			expect(result).to.deep.equal({data:[]});
 			done();
 		});
 	});
 
 	it('returns results if a non-ridiculous search term is provided', function (done) {
-		sfxHelper('medicine', function (result) {
+		sfx.search('medicine', function (result) {
 			expect(result.data).to.not.be.empty;
 			done();
 		});
 	});
 
 	it('returns an empty result if ridiculous search term is provided', function (done) {
-		sfxHelper('fhqwhgads', function (result) {
+		sfx.search('fhqwhgads', function (result) {
 			expect(result.data).to.be.empty;
 			done();
 		});
 	});
 
 	it('returns a single result for insanely specific search', function (done) {
-		sfxHelper('medicine and health, Rhode Island', function (result) {
+		sfx.search('medicine and health, Rhode Island', function (result) {
 			expect(result.data.length).to.equal(1);
 			done();
 		});
@@ -47,7 +43,7 @@ describe('sfx', function () {
 
 	it('returns an error object if there was an HTTP error', function (done) {
 		nock.disableNetConnect();
-		sfxHelper('medicine', function (result) {
+		sfx.search('medicine', function (result) {
 			nock.enableNetConnect();
 			expect(result.data).to.be.undefined;
 			expect(result.error).to.equal('Nock: Not allow net connect for "ucelinks.cdlib.org:8888"');
