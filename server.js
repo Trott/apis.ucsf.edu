e00559014e89541c9f9ede3d4c67f23e3a5fa793
Rich Trott <rtrott@gmail.com>
2014-10-28 08:57:08 -0700

    require node >=0.10.33 for poodle fix

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ var express = require('express'),
     sslCert = '/etc/pki/tls/certs/apis_ucsf_edu_cert.cer',
     caCert = '/etc/pki/tls/certs/incommon.crt';
 
-// Until NodeJS turns off SSLv3 by default https://github.com/joyent/node/pull/8551
-var constants = require('constants');
-
 var setIds = function () {
     'use strict';
     process.setgid(nodeUserGid);
@@ -31,21 +28,6 @@ try {
         key: fs.readFileSync(sslKey),
         cert: fs.readFileSync(sslCert),
         ca: fs.readFileSync(caCert),
-        // Until NodeJS turns off SSLv3 by default https://github.com/joyent/node/pull/8551
-        //
-        // This is the default secureProtocol used by Node.js, but it might be
-        // sane to specify this by default as it's required if you want to
-        // remove supported protocols from the list. This protocol supports:
-        //
-        // - SSLv2, SSLv3, TLSv1, TLSv1.1 and TLSv1.2
-        //
-        secureProtocol: 'SSLv23_method',
-
-        //
-        // Supply `SSL_OP_NO_SSLv3` constant as secureOption to disable SSLv3
-        // from the list of supported protocols that SSLv23_method supports.
-        //
-        secureOptions: constants.SSL_OP_NO_SSLv3,
     };
 } catch (e) {
     console.warn('Error setting HTTPS options: ' + e.message);
