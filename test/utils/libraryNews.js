e443ec77b8efe8aa2939e53b1019c7c9f4ad975d
Rich Trott <rtrott@gmail.com>
2014-12-18 13:59:27 -0800

    cruft removal

--- test/utils/libraryNews.js
+++ test/utils/libraryNews.js
@@ -1,6 +1,5 @@
 /*jshint expr: true*/
 
-// var rewire = require('rewire');
 var libraryNews = require('../../utils/libraryNews.js');
 
 var Code = require('code'); 
@@ -12,26 +11,11 @@ var describe = lab.experiment;
 var it = lab.test;
 
 var beforeEach = lab.beforeEach;
-// var afterEach = lab.afterEach;
 
 var nock = require('nock');
 
 
 describe('exports', function () {
-  // var reset;
-
-//   beforeEach(function (done) {
-//     nock.disableNetConnect();
-//     reset = librarySchedule.__set__({schedule: {}});
-//     done();
-//   });
-
-//   afterEach(function (done) {
-//     nock.cleanAll();
-//     reset();
-//     done();
-//   });
-
   it('should have properties for fetch()', function (done) {
     expect(libraryNews.fetch).to.be.a.function();
     done();
@@ -88,105 +72,4 @@ describe('exports', function () {
       libraryNews.fetch(callback);
     });
   });
-
-//   describe('update()', function () {
-//     it('should return the schedule in the expected format', function (done) {
-//       nock('http://api.libcal.com:80')
-//         .get('/api_hours_grid.php?iid=138&format=json&weeks=2')
-//         .replyWithFile(200, __dirname + '/../fixtures/hours.json');
-
-//       librarySchedule.once('update', function () {
-//         var schedule = librarySchedule.get();
-//         expect(schedule.locations).to.be.an.object();
-//         expect(schedule.locations.parnassus[0].day).not.to.contain('Invalid');
-//         expect(schedule.lastUpdated).to.be.a.number();
-//         done();
-//       });
-
-//       librarySchedule.update();
-//     });
-
-//     it('should log an error message if HTTP status code is not 200', function (done) {
-//       nock('http://api.libcal.com:80')
-//         .get('/api_hours_grid.php?iid=138&format=json&weeks=2')
-//         .reply(404, '404ed! Not found!');
-
-//       var mockLogger = function (logMsg) {
-//         expect(logMsg).to.equal('updateScheduleAsync error: code 404');
-//         done();
-//       };
-
-//       librarySchedule.update({logger: mockLogger});
-//     });
-
-//     it('should ignore entries with missing properties', function (done) {
-//       nock('http://api.libcal.com:80')
-//         .get('/api_hours_grid.php?iid=138&format=json&weeks=2')
-//         .replyWithFile(200, __dirname + '/../fixtures/borkedHours.json');
-
-//       librarySchedule.once('update', function () {
-//         var schedule = librarySchedule.get();
-//         expect(schedule.locations.parnassus[0]).to.deep.equal({
-//               'day': 'Sun',
-//               'date': 'Sep 21',
-//               'text': '12pm - 10pm'
-//             }
-//         );
-//         expect(schedule.locations.parnassus[1].text).to.equal('');
-//         done();
-//       });
-
-//       librarySchedule.update({date: '2014-09-21'});
-//     });
-
-//     it('should reject invalid JSON', function (done) {
-//       var logged = false;
-
-//       nock('http://api.libcal.com:80')
-//         .get('/api_hours_grid.php?iid=138&format=json&weeks=2')
-//         .reply(200, 'invalid JSON!');
-
-//       var mockLogger = function (logMsg) {
-//         expect(logMsg).to.equal('error parsing LibCal JSON: Unexpected token i');
-//         logged = true;
-//       };
-
-//       librarySchedule.once('update', function () {
-//         expect(logged).to.equal(true);
-//         expect(librarySchedule.get().locations).to.deep.equal({});
-//         done();
-//       });
-
-//       librarySchedule.update({logger: mockLogger});
-//     });
-
-//     it('should log an error if there is an HTTP error', function (done) {
-//       var mockLogger = function (logMsg) {
-//         expect(logMsg).to.equal('updateScheduleAsync error: Nock: Not allow net connect for "api.libcal.com:80"');
-//         done();
-//       };
-
-//       librarySchedule.update({logger: mockLogger});
-//     });
-//   });
-
-//   describe('get()', function () {
-//     it('should fetch hours for Mission Bay Hub', function (done) {
-//       nock('http://api.libcal.com:80')
-//         .get('/api_hours_grid.php?iid=138&format=json&weeks=2')
-//         .replyWithFile(200, __dirname + '/../fixtures/hours.json');
-
-//       librarySchedule.on('update', function () {
-//         var schedule = librarySchedule.get();
-//         expect(schedule.locations.missionBayHub.length).to.equal(7);
-//         schedule.locations.missionBayHub.forEach(function (value) {
-//           expect(value.text).to.equal('24 hours');
-//         });
-
-//         done();
-//       });
-
-//       librarySchedule.update({date: '2014-09-28'});
-//     });
-//   });
 });
